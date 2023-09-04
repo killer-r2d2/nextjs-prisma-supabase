5500466f008991b80d31ebb714e5ac612ecbb746
Roger Killer <roger.killer@advery.ch>
2023-09-04 19:46:24 +0200

    🎨 style(page.tsx): improve styling and layout of the Home page 🐛 fix(page.tsx): fix indentation and formatting issues in the Home page ✨ feat(page.tsx): add more descriptive headings and labels to improve readability 🔧 chore(page.tsx): refactor code to use more semantic HTML elements and CSS classes

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -23,30 +23,36 @@ export default function Home() {
   };
 
   return (
-    <main>
-      <h1>Create Post</h1>
-      <form onSubmit={(e) => {e.preventDefault(); submitPost();}}>
+    <main className="container mx-auto p-4">
+    <h1 className="text-3xl font-semibold mb-4">Create Post</h1>
+    <form onSubmit={(e) => {e.preventDefault(); submitPost();}} className="space-y-4">
+      <input
+        type="text"
+        placeholder="Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        className="w-full p-2 border rounded"
+      />
+      <textarea
+        placeholder="Content"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+        className="w-full p-2 border rounded h-32"
+      ></textarea>
+      <label className="flex items-center">
         <input
-          type="text"
-          placeholder="Title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
+          type="checkbox"
+          checked={published}
+          onChange={() => setPublished(!published)}
+          className="mr-2"
         />
-        <textarea
-          placeholder="Content"
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-        ></textarea>
-        <label>
-          <input
-            type="checkbox"
-            checked={published}
-            onChange={() => setPublished(!published)}
-          />
-          Published
-        </label>
-        <button type="submit">Create Post</button>
-      </form>
-    </main>
+        <span className="text-sm">Published</span>
+      </label>
+      <button type="submit" className="bg-blue-500 text-white p-2 rounded">
+        Create Post
+      </button>
+    </form>
+  </main>
+  
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -10,21 +10,29 @@ async function getPosts() {
 
 export default async function Home() {
   const data: { id: number; title: string }[] = await getPosts();
-    return (
-    <main className="container mx-auto">
-      <h1 className="text-2xl">lets test nextjs-prisma-supabase</h1>
-      <div className="border">
-        <h2 className="text-xl">show Posts from supabase:</h2>
-        {data.map((post) => (
-          <ul key={post.id}>
-            <li>{post.title}</li>
-          </ul>
-        ))}
+  return (
+    <main className="container mx-auto p-6">
+      <h1 className="text-4xl font-semibold mb-6">
+        Lets Test Next.js-Prisma-Supabase
+      </h1>
+
+      <div className="border p-4 rounded-lg mb-6">
+        <h2 className="text-2xl font-medium mb-4">Show Posts from Supabase:</h2>
+        <ul className="list-disc list-inside">
+          {data.map((post) => (
+            <li key={post.id} className="text-lg mb-2">
+              {post.title}
+            </li>
+          ))}
+        </ul>
       </div>
-      <div>
-        <h2>create a post and send it to supabase</h2>
-        <Link href="/create">
-          create a post
+
+      <div className="p-4 rounded-lg">
+        <h2 className="text-2xl font-medium mb-4">
+          Create a Post and Send it to Supabase
+        </h2>
+        <Link href="/create" className="text-blue-500 hover:underline">
+          Create a Post
         </Link>
       </div>
     </main>
